c379d15eac12ef42d9b42c838cb306d036286553
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-05-24 20:10:01 +0200

    Add files via upload

---
+++ Chapter9/Chapter9.js
@@ -0,0 +1,24 @@
+
+//const ele1 = document.querySelector("h1");
+//console.dir(ele1);
+
+//const eles = document.querySelectorAll(".output");
+//onsole.log(eles);
+
+//Window navigator object
+//console.dir(window.navigator);
+
+//console.dir(navigator);
+
+//Window location object
+//location.ancestorOrigins.length;
+
+
+//console.log(location.protocol);
+
+//Practice exercise 9.2
+//console.log(location);
+//console.log(location.protocol);
+
+//const ele1 = document.querySelector("h1");
+//console.dir(ele1);
