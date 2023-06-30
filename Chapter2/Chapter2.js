4499c5a651b7d6a645763958444ec81eff654fb9
lihle-sibiya <129907260+lihle-sibiya@users.noreply.github.com>
2023-06-30 23:20:22 +0200

    Add files via upload

--- Chapter2/Chapter2.js
+++ Chapter2/Chapter2.js
@@ -146,18 +146,17 @@
 // console.log(nr1 + Number(nr2));
 
 //Practice exercise 2.1
-// let str1 = 'Laurence'; 
-// let str2 = "Svekis"; 
-// let val1 = undefined;
-// let val2 = null;
-// let myNum = 1000;
+/*let str1 = 'Laurence'; 
+let str2 = "Svekis"; 
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
 
-
-// console.log(typeof(str1));
-// console.log(typeof(str2));
-// console.log(typeof(val1));
-// console.log(typeof(val2));
-// console.log(typeof(myNum));
+console.log(typeof(str1));
+console.log(typeof(str2));
+console.log(typeof(val1));
+console.log(typeof(val2));
+console.log(typeof(myNum));*/
 
 //Arithmetic operators
 //Addition
@@ -170,11 +169,11 @@
 // let result2 = str1 + str2;
 
 //Practice exercise 2.2
-// const myName = "Maaike";
-// const myAge = 29;
-// const coder = true;
-// const message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
-// console.log(message);
+/*const myName = "Maaike";
+const myAge = 29;
+const coder = true;
+const message = "Hello, my name is " + myName + ", I am " + myAge+" years old and I can code JavaScript: " + coder + "."; 
+console.log(message);*/
 
 
 //Subtraction
@@ -230,12 +229,12 @@
 // console.log(nr2);
 
 //Practice exercise 2.3
-// let a = window.prompt("Value 1?");
-// let b = window.prompt("Value 2?");
-// a = Number(a);
-// b = Number(b);
-// let hypotenuseVal = ((a * a) + (b * b))**0.5;
-// console.log(hypotenuseVal);
+/*let a = window.prompt("Value 1?");
+let b = window.prompt("Value 2?");
+a = Number(a);
+b = Number(b);
+let hypotenuseVal = ((a * a) + (b * b))**0.5;
+console.log(hypotenuseVal);*/
 
 
 //Practice exercise 2.4
@@ -247,4 +246,21 @@
 // c = c % b;
 // console.log(a, b, c);
 
+//Chapter project
+//1. Convert miles to kilometers. 
+//***1 mile equals 1.60934 kilometers.
+// let myDistanceMiles = 130;
+// let myDistanceKM = myDistanceMiles * 1.60934;
+// console.log("The distance of " + myDistanceMiles + " miles is equal to " + myDistanceKM + " kilometers");
+
+//2 BMI calculator
+//***1 inch = 2.54 centimetres.
+//***2.2046 pounds in a kilo
+// let inches = 72;
+// let pounds = 180;
+// let weight = pounds / 2.2046; // in kilos
+// let height = inches * 2.54; // height in centimetres
+// console.log(weight, height);
+// let bmi = weight/(height/100*height/100);
+// console.log(bmi);
 
--- Chapter3/Exercise_3.2.js
+++ Chapter3/Exercise_3.2.js
@@ -1,12 +1,33 @@
+//1. Create an empty array to use as a shopping list.
 /*const myList = [];
+
+//2. Add Milk, Bread, and Apples to your list.
 myList.push("Milk", "Bread", "Apples");
+
+//3. Update "Bread" with Bananas and Eggs.
 myList.splice(1, 1, "Bananas", "Eggs");
+
+//4. Remove the last item from the array and output it into the console.
 const removeLast = myList.pop();
 console.log(removeLast);
+
+//5. Sort the list alphabetically.
 myList.sort();
+
+//6. Find and output the index value of Milk.
 console.log(myList.indexOf("Milk"));
+
+//7. After Bananas, add Carrots and Lettuce.
 myList.splice(1, 0, "Carrots", "Lettuce");
+
+//8. Create a new list containing Juice and Pop.
 const myList2 = ["Juice", "Pop"];
+
+//9. Combine both lists, adding the new list twice to the end of the first list.
 const finalList = myList.concat(myList2, myList2);
+
+//10. Get the last index value of Pop and output it to the console.
 console.log(finalList.lastIndexOf("Pop"));
+
+//11. Your final list should look like this:
 console.log(finalList);*/
